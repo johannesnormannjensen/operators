38d48272c8c2295721fba17d9712fdc5ab8c365e
Johannes <johannes@nestech.dk>
2020-03-20 09:40:56 +0100

    Add code preview

--- libs/scan/src/lib/accumulate-object/accumulate-object.component.ts
+++ libs/scan/src/lib/accumulate-object/accumulate-object.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { scan } from 'rxjs/operators';
 
-@Component({
-  selector: 'operators-accumulate-object',
-  templateUrl: './accumulate-object.component.html',
-  styleUrls: ['./accumulate-object.component.css']
-})
-export class AccumulateObjectComponent implements OnInit {
-
+const code = `
   key = 'name';
   value = 'Brandon';
 
@@ -25,8 +19,38 @@ export class AccumulateObjectComponent implements OnInit {
   }
 
   addObject() {
-    console.log({[this.key]: this.value});
     this.objSource$.next( {[this.key]: this.value});
   }
+`;
+
+@Component({
+  selector: 'operators-accumulate-object',
+  templateUrl: './accumulate-object.component.html',
+  styleUrls: ['./accumulate-object.component.css']
+})
+export class AccumulateObjectComponent implements OnInit {
+
+  code = code;
+
+  key = 'name';
+  value = 'Brandon';
+
+  objSource$ = new Subject<any>();
+
+  example$: Observable<any>;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.example$ = this.objSource$.pipe(
+      scan((acc, curr) => Object.assign({}, acc, curr), {})
+    );
+  }
+
+  addObject() {
+    this.objSource$.next({ [this.key]: this.value });
+  }
+
 
 }
