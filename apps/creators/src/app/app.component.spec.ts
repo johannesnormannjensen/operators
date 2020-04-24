3a23d7cee41e9f53b25fdecaa066ebb6cf7e8449
Johannes <johannes@nestech.dk>
2020-04-24 11:06:00 +0200

    fix tests

--- apps/creators/src/app/app.component.spec.ts
+++ apps/creators/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PureUiModule } from '@operators/pure-ui';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, PureUiModule],
       declarations: [AppComponent]
     }).compileComponents();
   }));
