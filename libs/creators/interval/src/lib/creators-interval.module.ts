5ea6b73d9084b2bc25abd85b6c6c78e8dbb8faee
Johannes <johannes@nestech.dk>
2020-04-24 10:07:01 +0200

    Add interval component

--- libs/creators/interval/src/lib/creators-interval.module.ts
+++ libs/creators/interval/src/lib/creators-interval.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { IntervalComponent } from './interval/interval.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'interval'
+      },
+      {
+        path: 'interval',
+        component: IntervalComponent
+      },
+    ]),
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [IntervalComponent],
 })
 export class CreatorsIntervalModule {}
